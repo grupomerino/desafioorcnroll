dcfbc2eff60bf4eb272ffdd062493c0a091fa9e6
Carlos França Júnior <francasense@gmail.com>
2019-10-22 01:31:16 -0300

    primeiro commit

---
+++ build/precache-manifest.5c7f5a2a861aa3fd83798174663e0661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa98c39a0d5fea197134335d02f22f57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f6a822e32a6873b5d47",
+    "url": "/static/css/main.c565472e.chunk.css"
+  },
+  {
+    "revision": "6e768af2f72934111ef8",
+    "url": "/static/js/2.00b071c7.chunk.js"
+  },
+  {
+    "revision": "4f6a822e32a6873b5d47",
+    "url": "/static/js/main.93676a86.chunk.js"
+  },
+  {
+    "revision": "f96a880c3662ffd63088",
+    "url": "/static/js/runtime-main.c9309d36.js"
+  }
+]);
